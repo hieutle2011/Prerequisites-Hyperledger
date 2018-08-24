5e8a6e277fd1cd61fa0b6dd8ac2c49c59334213f
hieutle2011 <30904297+hieutle2011@users.noreply.github.com>
2018-08-24 18:12:44 +0700

    Update cd

--- composer-prereqs.sh
+++ composer-prereqs.sh
@@ -12,6 +12,7 @@
 #    git: 2.9.x or higher
 #    Python: 2.7.x
 #    A code editor of your choice, we recommend VSCode.
+cd 
 
 curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
 
